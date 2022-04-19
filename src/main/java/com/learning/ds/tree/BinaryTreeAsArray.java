8253485756417b82de8580202f386b99e0bebf9b
Gaurav Handa <gauravhanda21@yahoo.com>
2022-04-18 23:57:37 -0500

    Added node count

--- src/main/java/com/learning/ds/tree/BinaryTreeAsArray.java
+++ src/main/java/com/learning/ds/tree/BinaryTreeAsArray.java
@@ -9,9 +9,17 @@ public class BinaryTreeAsArray {
     int nodeCount = 0;
 
     public BinaryTreeAsArray(int height) {
-        this.tree = new int[(int) Math.pow(2, height + 1) + 1];
+        /**
+         * Number of nodes in Binary tree are 2* height - 1
+         */
+        this.tree = new int[(int) Math.pow(2, height + 1) - 1];
     }
 
+    /**
+     * Start with zero and then try to insert the less value to (2*n + 1) and greater value to (2*n + 2)
+     *
+     * @param value
+     */
     private void add(int value) {
         if (nodeCount == 0) {
             tree[0] = value;
@@ -26,6 +34,7 @@ public class BinaryTreeAsArray {
                 if (tree[currentIndex] == 0) {
                     System.out.println("Insertion location " + currentIndex);
                     tree[currentIndex] = value;
+                    nodeCount++;
                     targetToInsertFound = true;
                 }
             }
@@ -41,12 +50,8 @@ public class BinaryTreeAsArray {
     }
 
     private void printTree() {
+        System.out.println("Total nodes in tree " + nodeCount);
         Arrays.stream(tree).forEach(System.out::println);
-    }
-
-    @Test
-    public void leftOnlyTree() {
-
     }
 
     public static void main(String[] args) {
